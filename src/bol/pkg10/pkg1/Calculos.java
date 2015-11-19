3bea04e8e4a68939dfbb1c948d3ad60af7fa622e
galle <galle@192.168.0.16>
2015-11-19 15:47:33 +0100

    Variables renombradas

--- src/bol/pkg10/pkg1/Calculos.java
+++ src/bol/pkg10/pkg1/Calculos.java
@@ -8,37 +8,38 @@ import javax.swing.JOptionPane;
  */
 public class Calculos {
 
-    int numeroAd;
-    int numeroMax;
-    
+    int numeroAdivinar;
+    int numeroIntentos;
+
     public int introduccionNumero() {
         do {
-            numeroAd = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1: Introduce un numero entre 1 y 50 a adivinar"));
-        } while (numeroAd < 1 | numeroAd > 50);
-        return numeroAd;
+            numeroAdivinar = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1: Introduce un numero entre 1 y 50 a adivinar"));
+        } while (numeroAdivinar < 1 | numeroAdivinar > 50);
+        return numeroAdivinar;
     }
 
     public int introduccionIntentos() {
-        numeroMax = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1: Introduce numero de intentos para el jugador 2"));
-        return numeroMax;
+        numeroIntentos = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1: Introduce numero de intentos para el jugador 2"));
+        return numeroIntentos;
     }
 
     public void comparar() {
-        
-        numeroMax = introduccionIntentos();
-        numeroAd = introduccionNumero();
-        for (int i = 0; i < numeroMax; i++) {
+
+        numeroIntentos = introduccionIntentos();
+        numeroAdivinar = introduccionNumero();
+        for (int i = 0; i < numeroIntentos; i++) {
             int numRespuesta = Integer.parseInt(JOptionPane.showInputDialog("Jugador 2: Adivina un numero entre el 1 y el 50"));
-            if (numRespuesta > numeroAd) {
-                JOptionPane.showMessageDialog(null, "Te has pasado");
-            } else if (numRespuesta < numeroAd) {
-                JOptionPane.showMessageDialog(null, "Te has quedado corto");
-            }
-            else if (numRespuesta == numeroAd) {
+            if (numRespuesta > numeroAdivinar) {
+                JOptionPane.showMessageDialog(null, "¡¡¡INCORRECTO!!! Te has pasado");
+            } 
+            else if (numRespuesta < numeroAdivinar) {
+                JOptionPane.showMessageDialog(null, "¡¡¡INCORRECTO!!! Te has quedado corto");
+            } 
+            else if (numRespuesta == numeroAdivinar) {
                 JOptionPane.showMessageDialog(null, "Has acertado");
                 break;
             }
-            
+
         }
     }
 }
